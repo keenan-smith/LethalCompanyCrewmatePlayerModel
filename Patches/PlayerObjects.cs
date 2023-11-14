aa942bfbfbd05e18d847302b367a28ed6cdb59b8
Keenan Smith <keenan.smith@uconn.edu>
2023-11-14 17:56:53 -0500

    Update hook

--- Patches/PlayerObjects.cs
+++ Patches/PlayerObjects.cs
@@ -1,18 +1,46 @@
 using GameNetcodeStuff;
 using HarmonyLib;
+using System.Diagnostics;
+using System.Numerics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace AmogusModels.Patches
 {
     [HarmonyPatch]
     internal class PlayerObjects
     {
-        [HarmonyPatch(typeof(PlayerControllerB), "Start")]
-        [HarmonyPostfix]
-        public static void InitModels(ref PlayerControllerB __instance)
+        public static void InitModels()
         {
-            if (__instance.gameObject.name != "Player")
+            var localPlayer = GameNetworkManager.Instance.localPlayerController;
+            Debug.Log(localPlayer.gameObject.name);
+            var players = UnityEngine.Object.FindObjectsOfType<PlayerControllerB>();
+            foreach (var player in players)
             {
-                __instance.gameObject.AddComponent<AmogusController>();
+                if (player == localPlayer) continue;
+                player.gameObject.AddComponent<AmogusController>();
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB), "SpawnPlayerAnimation")]
+        [HarmonyPostfix]
+        public static void InitModel(ref PlayerControllerB __instance)
+        {
+            InitModels();
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB), "DisablePlayerModel")]
+        [HarmonyPostfix] 
+        public static void DisablePlayerModel(ref PlayerControllerB __instance, GameObject playerObject)
+        {
+            var localPlayer = GameNetworkManager.Instance.localPlayerController;
+            if (playerObject == localPlayer) return;
+            playerObject.gameObject.GetComponentInChildren<LODGroup>().enabled = false;
+            var meshes = playerObject.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (var LODmesh in meshes)
+            {
+                if (LODmesh.name == "Body") continue;
+                LODmesh.enabled = false;
             }
         }
     }
